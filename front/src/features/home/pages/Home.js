bbd2aa509aaf001b56b220210d41fba47eb7fb7c
imananjara <ivan.mananjara@gmail.com>
2022-10-27 16:56:57 +0200

    Add questionnaire form

--- front/src/features/home/pages/Home.js
+++ front/src/features/home/pages/Home.js
@@ -1,20 +1,21 @@
 import homeImg from '../../../assets/home-img.png';
+import { Link } from 'react-router-dom';
 
 
 export default function Home() {
     return <div className="home">
         <div className="home-left-block">
             <h1 className="home-title">
-                DECOUVREZ EN 8 QUESTIONS LES PLANTES 
+                DECOUVREZ EN 8 QUESTIONS LES PLANTES
                 QUI SONT FAITES POUR VOUS !
             </h1>
-            <button className="home-btn">
-                JE COMMENCE LE QUESTIONNAIRE
-            </button>
 
+            <Link to="/questionnaire">
+                <button className="home-btn">
+                    JE COMMENCE LE QUESTIONNAIRE
+                </button>
+            </Link>
         </div>
-        <img src={homeImg} className="home-img"/>
-
-
+        <img src={ homeImg } alt="home_icon" className="home-img"/>
     </div>;
 }
---
+++ front/src/features/questionnaire/pages/Questionnaire.js
@@ -0,0 +1,54 @@
+import { formStepModel } from '../models/form-step-model.js';
+import { useNavigate } from 'react-router-dom';
+import React, { useState } from 'react';
+
+export default function Questionnaire() {
+    const navigate = useNavigate();
+    const [formStepState/*, setFormStepState*/] = useState(formStepModel);
+    let [step, setStep] = useState(0);
+
+    const buildSearchRqst = (formStepState) => {
+        let bodySearch = {};
+        formStepState.forEach(s => {
+            bodySearch[s.questionCode] = s.selectedResponseValue;
+        });
+        console.log('bodySearch ',bodySearch);
+        return bodySearch;
+    };
+
+    const navigateToResult = (formStepState) => {
+        const searchRqst = buildSearchRqst(formStepState);
+        navigate("/result", { searchRqst });
+    };
+
+    const saveStepResponseAndGoToTheNextStep = (response) => {
+        formStepState[step].selectedResponseValue = response.value;
+        step = step + 1;
+        setStep(step);
+        if (step === formStepState.length) {
+            navigateToResult(formStepState);
+        }
+    };
+
+    const renderResponse = (response) => {
+        return <div key={response.value} className="response">
+            <div className="img">
+
+            </div>
+            <button className="response-btn" onClick={() => saveStepResponseAndGoToTheNextStep(response)}>
+                {response.label}
+            </button>
+        </div>
+    };
+
+    const currentStep = formStepState[step];
+
+    return currentStep ? <div className="questionnaire-form-container">
+        <h1 className="step-question">
+            {currentStep.questionLabel}
+        </h1>
+        <div className="step-responses">
+            {currentStep.responses.map(renderResponse)}
+        </div>
+    </div> : null;
+}
