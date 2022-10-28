b84454395e9d3c0c1a049f5c4e5a421c6e0d84c7
imananjara <ivan.mananjara@gmail.com>
2022-10-28 13:24:10 +0200

    Add animations + update header logo

--- front/src/App.js
+++ front/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes, Navigate, Link, useNavigate } from 'react-router-dom';
 import Home from './features/home/pages/Home.js'
