d40cb4d7dfc2f755776b9a2ec3dda56a49fb6f8d
Riad KECHIR <kechir@SNEW.localdomain>
2022-10-28 11:00:33 +0200

    Go to home

--- front/src/App.js
+++ front/src/App.js
@@ -1,21 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate, Link } from 'react-router-dom';
 import Home from './features/home/pages/Home.js'
 import Questionnaire from './features/questionnaire/pages/Questionnaire';
 
 function App() {
   return (
     <div className="app-container">
-      <div className="header">
-        <img src={ logo } alt="green_house_logo" />
-        <menu>
-          <li>Blog</li>
-          <li>Nos partenaires</li>
-          <li>En savoir +</li>
-        </menu>
-      </div>
       <Router>
+        <div className="header">
+          <Link to="/"><img src={logo} alt="green_house_logo" /></Link>
+          <menu>
+            <li>Blog</li>
+            <li>Nos partenaires</li>
+            <li>En savoir +</li>
+          </menu>
+        </div>
         <Routes>
           <Route path="/" element={<Navigate to="/home" replace />} />
           <Route path="/home" element={<Home />} />
